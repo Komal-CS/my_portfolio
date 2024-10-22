ce54f9f3cc872e226391f60c77bdf3b362e3b5ed
Komal Joshi <86408084+Komal-CS@users.noreply.github.com>
2024-10-22 19:05:52 +0530

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,21 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "Results-driven software developer adept in full-stack development, specializing in JavaScript, React, and
+Node.js. Eager to apply technical expertise and creativity in contributing to innovative projects and
+collaborating within dynamic teams.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Programming Languages: Python, Java, PHP, HTML, CSS, JavaScript",
+  "Frameworks & Libraries: Django, Node.js, Express.js, Ruby on Rails",
+  "Machine Learning: Naïve Bayes, K-Means Clustering, Deep Learning Concepts",
+  "Web Development: Frontend (HTML, CSS, JavaScript), Backend (PHP, Django)",
+  "API Integration: Azure Functions, REST APIs",
+  "Graphic Design: Canva, Adobe Tools",
 ];
 
 /**
@@ -45,7 +47,9 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "Results-driven software developer adept in full-stack development, specializing in JavaScript, React, and
+Node.js. Eager to apply technical expertise and creativity in contributing to innovative projects and
+collaborating within dynamic teams.";
 
 const About = () => {
   return (
